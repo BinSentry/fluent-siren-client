a4ccfea1acf421fdf2e199681be546f74166f606
ruben <ruben@binsentry.com>
2020-02-14 10:45:42 -0500

    Change _perform() function to be private
    Also do some JSDoc improvements

--- index.js
+++ index.js
@@ -12,26 +12,25 @@ class Client {
    *  body: response body
    *  contentType: Content-Type response header
    *
-   * @typedef response
+   * @typedef {Object} Response
    * @property body response body
    * @property contentType Content-Type response header
    *
    * @callback requestFn
    * @param {string} href (required) href of the resource
    * @param {string} method (required) HTTP request verb ('GET', 'POST', 'PUT', 'PATCH', 'DELETE')
-   * @param {object} [fieldValues] (optional) field names and values for the request
-   * @return {response} body: response body, contentType: Content-Type response header
+   * @param {Object} [fieldValues] (optional) field names and values for the request
+   * @return {Response} body: response body, contentType: Content-Type response header
    */
 
   /**
    * Setup the fluent siren client
    *
-   * @typedef options
-   * @type {object}
-   * @property {String|String[]} [originWhitelist] (optional) Whitelist of allowed origins (e.g. 'https://example.com')
+   * @typedef {Object} Options
+   * @property {string|string[]} [originWhitelist] (optional) Whitelist of allowed origins (e.g. 'https://example.com')
    *
    * @param {requestFn} requestFn (required) performs an async HTTP request
-   * @param {options} [options] (optional) configuration options for the client
+   * @param {Options} [options] (optional) configuration options for the client
    */
   constructor(requestFn, options = {}) {
     this._requestFn = requestFn;
@@ -40,7 +39,7 @@ class Client {
 
   /**
    * Starts a client by fetching the provided href
-   * @param href
+   * @param {string} href
    * @returns {Promise<Entity>}
    */
   async start(href) {
@@ -49,28 +48,20 @@ class Client {
 
   /**
    * Performs a request
-   * @param href
-   * @param method
-   * @param [fieldValues]
+   * @param {string} href
+   * @param {string} method
+   * @param {Object} [fieldValues]
    * @returns {Promise<Entity>}
    */
   async perform(href, method, fieldValues) {
-    verifyOrigin(href, this._options.originWhitelist);
-    return this._perform(href, method, fieldValues);
+    _verifyOrigin(href, this._options.originWhitelist);
+    return _perform(this, href, method, fieldValues);
   }
 
-  async _perform(href, method, fieldValues) {
-    const { body, contentType } = await this._requestFn(href, method, fieldValues);
-    if (!body || !contentType || !_isSiren(contentType)) {
-      return body;
-    }
-
-    const entity = new Entity(body);
-    this.attachClient(entity);
-
-    return entity;
-  }
-
+  /**
+   * Attaches this client to an existing entity
+   * @param {Entity} entity
+   */
   attachClient(entity) {
     entity._client = this;
     for (const subEntity of entity.entities || []) {
@@ -87,7 +78,19 @@ class Client {
 
 module.exports = Client;
 
-function verifyOrigin(href, originWhitelist) {
+async function _perform(client, href, method, fieldValues) {
+  const { body, contentType } = await client._requestFn(href, method, fieldValues);
+  if (!body || !contentType || !_isSiren(contentType)) {
+    return body;
+  }
+
+  const entity = new Entity(body);
+  client.attachClient(entity);
+
+  return entity;
+}
+
+function _verifyOrigin(href, originWhitelist) {
   if (!originWhitelist) {
     return;
   }
@@ -110,17 +113,17 @@ function _isSiren(contentType) {
 
 Entity.prototype.follow = async function () {
   const self = this.getLinkByRel('self');
-  return this._client._perform(self.href, 'GET');
+  return _perform(this._client, self.href, 'GET');
 };
 
 Link.prototype.follow = async function () {
-  return this._client._perform(this.href, 'GET');
+  return _perform(this._client, this.href, 'GET');
 };
 
 /**
  * Perform this action with the given fieldValues
  *
- * @param {object} fieldValues field values { fieldName: value }
+ * @param {Object} fieldValues field values { fieldName: value }
  */
 Action.prototype.perform = async function (fieldValues) {
   const hiddenFields = this.fields && this.fields.filter(field => field.type.toLowerCase() === 'hidden') || [];
@@ -128,5 +131,5 @@ Action.prototype.perform = async function (fieldValues) {
     values[field.name] = field.value;
     return values;
   }, {});
-  return this._client._perform(this.href, this.method, { ...hiddenValues, ...fieldValues });
+  return _perform(this._client, this.href, this.method, { ...hiddenValues, ...fieldValues });
 };
